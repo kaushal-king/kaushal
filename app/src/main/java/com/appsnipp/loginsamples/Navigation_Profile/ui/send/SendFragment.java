87cc978d46164c3db5ad724973a9ccc90c2bf48b
kaushik2001 <mokariyakaushik2001@gmail.com>
2019-09-17 19:18:20 +0530

    navigationSECOND

---
+++ app/src/main/java/com/appsnipp/loginsamples/Navigation_Profile/ui/send/SendFragment.java
@@ -0,0 +1,34 @@
+package com.appsnipp.loginsamples.Navigation_Profile.ui.send;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.appsnipp.loginsamples.R;
+
+public class SendFragment extends Fragment {
+
+    private SendViewModel sendViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        sendViewModel =
+                ViewModelProviders.of(this).get(SendViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+        final TextView textView = root.findViewById(R.id.text_send);
+        sendViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
